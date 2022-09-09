acb1dd31b8d17259575880fda1ff7298eaad45e8
Alexandre Cordeiro Trapp <alexandre.trapp@warren.com.br>
2022-09-09 17:33:35 -0300

    Add project files.

---
+++ Algorithms/BinarySearch.cs
@@ -0,0 +1,30 @@
+﻿namespace TrappDSA.Algorithms;
+
+public static class BinarySearch
+{
+    public static void Execute()
+    {
+        var ordenedArray = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        var targetNumber = int.Parse(Console.ReadLine()!);
+
+        var middle = ordenedArray.Length / 2;
+        Console.WriteLine($"Findind target number");
+
+        while (middle >= 0)
+        {
+            Console.WriteLine($"ordenedArray[{middle}] => {ordenedArray[middle]}");
+
+            if (ordenedArray[middle] == targetNumber)
+            {
+                Console.WriteLine($"TargetNumber {targetNumber} founded on position ordenedArray[{middle}]");
+                return;
+            }
+
+            if (ordenedArray[middle] > targetNumber)
+                middle--;
+            else
+                middle++;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿using TrappDSA.Algorithms;
+
+BinarySearch.Execute();
